bd4610b8d5867a8511859e2c62c5ef115a89b6bd
Robbert Broersma <git@robbertbroersma.nl>
2025-03-17 18:22:53 +0100

    feat: add table of contents

---
+++ src/index-page.ts
@@ -0,0 +1,34 @@
+const showAllFigures = (url) =>
+  fetch(url).then((response) => {
+    response.text().then((svg) => {
+      const doc = new DOMParser().parseFromString(svg, "application/xml");
+      Array.from(doc.querySelectorAll("element"))
+        .filter((el) => el.getAttribute("xsi:type") === "archimate:ArchimateDiagramModel")
+        .map((el) => ({
+          id: el.getAttribute("id"),
+          name: el.getAttribute("name"),
+          element: el,
+        }))
+        .forEach(({ id, name }) => {
+          const section = document.createElement("section");
+          const heading = section.appendChild(document.createElement("h2"));
+          heading.textContent = name;
+
+          if (id) {
+            heading.id = id;
+
+            heading.textContent = "";
+            const anchorLink = document.createElement("a");
+            anchorLink.href = `#${id}`;
+            anchorLink.textContent = name;
+            heading.appendChild(anchorLink);
+          }
+          const archimateImg = document.createElement("archimate-img");
+          archimateImg.setAttribute("src", `${url}#${id}`);
+          section.appendChild(archimateImg);
+          document.body.appendChild(section);
+        });
+    });
+  });
+
+showAllFigures("./nl-design-system.archimate");
