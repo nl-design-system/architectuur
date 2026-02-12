e8c14d8e34c37842744260e63f1437f85d3a9e4e
Richard van Heuven van Staereling <richard@vhvs.nl>
2026-02-12 12:50:43 +0100

    chore: patch round 2026-01 (#22)
    Co-authored-by: Richard van Heuven van Staereling <richadr@users.noreply.github.com>

---
+++ .ncurc.patch.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  cooldown: 1, // 1 day
+  dep: ['dev', 'prod'],
+  install: 'always',
+  reject: [],
+  root: true,
+  target: 'patch',
+  upgrade: true,
+  workspaces: false,
+};
